7619af301b019280d9d93315045b16bcdb740f5c
Daniel <dfpires@gmail.com>
2025-08-25 10:23:12 -0300

    exe1 da Lista 3

---
+++ Lista3/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int contador = 1;
+        float nota;
+        Scanner sc = new Scanner(System.in);
+        float soma = 0;
+        while (contador <= 5){
+            System.out.println("Informe a nota " + contador);
+            nota = sc.nextFloat();
+            soma += nota;
+            contador++;
+        }
+        float media = soma / 5;
+        System.out.println("Media: " + media);
+    }
+}

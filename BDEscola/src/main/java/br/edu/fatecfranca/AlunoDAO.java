54b03fc355d8be05aa455e5afff271786c7d3216
Daniel <dfpires@gmail.com>
2025-10-21 09:16:03 -0300

    Atualiza aluno

--- BDEscola/src/main/java/br/edu/fatecfranca/AlunoDAO.java
+++ BDEscola/src/main/java/br/edu/fatecfranca/AlunoDAO.java
@@ -42,12 +42,65 @@ public class AlunoDAO {
             while(rs.next()){ // para cada aluno
                 // adiciona id e nome na lista de alunos
                 alunos.add(rs.getInt("id") + " - " +
-                        rs.getString("nome"));
+                        rs.getString("nome") + " - " +
+                        rs.getString("cpf"));
             }
         }
         catch(SQLException e){ // tivemos um erro
             System.out.println("Erro ao listar alunos!" + e.getMessage());
         }
         return alunos; // retorna a lista de alunos
+    }
+    // função para remoção de um aluno no banco de dados
+    public void remover(int id){
+        String sql = "DELETE FROM aluno WHERE id = ?";
+        // tenta remover
+        try {
+            // obtem a conexão com o banco de dados
+            Connection conexao = ConnectionFactory.getConnection();
+            // cria uma instrução de banco de dados
+            PreparedStatement instrucao = conexao.prepareStatement(sql);
+            // define o valor do id
+            instrucao.setInt(1, id);
+            // executa a remoção
+            int nroLinhasAfetadas = instrucao.executeUpdate();
+            // testa a qtde de linhas afetadas
+            if (nroLinhasAfetadas > 0){
+                System.out.println("Aluno removido com sucesso!");
+            }
+            else {
+                System.out.println("Aluno não encontrado");
+            }
+        }
+        catch(SQLException e){ // deu erro ao remover ou ao conectar com o banco
+            System.out.println("Erro ao remover aluno " + e.getMessage());
+        }
+    }
+    // função para atualizar um aluno no banco de dados
+    public void atualizar(int id, String novoNome, String novoCpf){
+        // monta o SQL
+        String sql = "UPDATE aluno SET nome = ?, cpf = ? WHERE id = ?";
+        // tenta atualizar o aluno
+        try {
+            // abre a conexão com o banco
+            Connection conexao = ConnectionFactory.getConnection();
+            // cria uma instrução (statement)
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            // atribuir os valores (?)
+            stmt.setString(1, novoNome);
+            stmt.setString(2, novoCpf);
+            stmt.setInt(3, id);
+            // executa o comando de atualização
+            int nroLinhasAfetadas = stmt.executeUpdate();
+            if (nroLinhasAfetadas > 0){
+                System.out.println("Aluno atualizado com sucesso!");
+            }
+            else {
+                System.out.println("Aluno não encontrado");
+            }
+        }
+        catch(SQLException e){
+            System.out.println("Erro ao atualizar aluno " + e.getMessage());
+        }
     }
 }
--- BDEscola/src/main/java/br/edu/fatecfranca/App.java
+++ BDEscola/src/main/java/br/edu/fatecfranca/App.java
@@ -1,24 +1,44 @@
 package br.edu.fatecfranca;
 
-import java.util.List;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-
-        AlunoDAO alunoDAO = new AlunoDAO();
-        alunoDAO.inserir("Pedro José", "675443");
-        List<String> alunos = alunoDAO.listar();
-        System.out.println("Lista de Alunos: ");
-        for (String aluno : alunos) { // para cada aluno da lista alunos
-            System.out.println(aluno); // exibe id e nome do aluno
+public class App {
+    public static void main( String[] args ){
+        AlunoDAO dao = new AlunoDAO();
+        Scanner entrada = new Scanner(System.in);
+        int opcao;
+        do {
+            System.out.println("Menu Aluno");
+            System.out.println("1 - Cadastrar Aluno");
+            System.out.println("2 - Alterar Aluno");
+            System.out.println("3 - Excluir Aluno");
+            System.out.println("4 - Listar Alunos");
+            System.out.println("0 - Sair");
+            System.out.println("Escolha: ");
+            opcao = entrada.nextInt();
+            switch(opcao){
+                case 1:
+                    System.out.println("Informe nome do aluno");
+                    String nome = entrada.nextLine();
+                    System.out.println("Informe cpf do aluno");
+                    String cpf = entrada.nextLine();
+                    dao.inserir(nome, cpf);
+                    break;
+                case 2:
+                    System.out.println("Informe id do aluno");
+                    int id = entrada.nextInt();
+                    System.out.println("Informe nome do aluno");
+                    nome = entrada.nextLine();
+                    System.out.println("Informe cpf do aluno");
+                    cpf = entrada.nextLine();
+                    dao.atualizar(id, nome, cpf);
+                    break;
+                case 3:
+                    System.out.println("Informe id do aluno");
+                    id = entrada.nextInt();
+                    dao.remover(id);
+            }
         }
-
-
+        while (opcao != 0);
     }
 }
